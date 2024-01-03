6b8b886b390d931f83b72aea07216153bbc41863
Luca Di Stefano <lucad@chalmers.se>
2024-01-03 20:18:22 +0100

    Flag LTOL in wrong places
    As a result of 2655da5f33848bd7ae70670698e72303cc013f6a, we can now
    mix LTOL and "basic" expressions quite liberally. So we run a validator
    that marks LTOL formulas as errors basically everywhere but after a
    SPEC.

--- src/language/r-check-validator.ts
+++ src/language/r-check-validator.ts
@@ -1,5 +1,5 @@
-import { ValidationAcceptor, ValidationChecks } from 'langium';
-import { Agent, Model, RCheckAstType } from './generated/ast.js';
+import { AstNode, ValidationAcceptor, ValidationChecks, isAstNode, streamAst, streamContents } from 'langium';
+import { Agent, Model, RCheckAstType, isAssign, isBinExpr, isBox, isCommand, isDiamond, isExpr, isFinally, isGlobally, isNext } from './generated/ast.js';
 import type { RCheckServices } from './r-check-module.js';
 
 /**
@@ -15,10 +15,25 @@ export function registerValidationChecks(services: RCheckServices) {
     registry.register(checks, validator);
 }
 
+function checkForLtol(node: AstNode, accept: ValidationAcceptor): void {
+    if (!isAstNode(node)) return;
+    if (
+        isDiamond(node) || isBox(node)
+        || isFinally(node) || isGlobally(node) || isNext(node)
+        || (isBinExpr(node) && "RUW".indexOf(node.operator) != -1)
+    ) {
+        accept("error", "LTOL not allowed here", {node: node})
+    }
+    if (isExpr(node)) {
+        streamContents(node).forEach(n => checkForLtol(n, accept));
+    }
+}
+
 /**
  * Implementation of custom validations.
  */
 export class RCheckValidator {
+    
     checkAgent(agent: Agent, accept: ValidationAcceptor): void {
         const locals = new Set();
         agent.locals?.forEach(v => {
@@ -27,17 +42,28 @@ export class RCheckValidator {
             }
             locals.add(v.name);
         });
+        if (agent.init !== undefined){
+            checkForLtol(agent.init, accept);
+        }
+        for (const n of streamAst(agent)){
+            if (isAssign(n) || isCommand(n)) {
+                streamContents(n).forEach(m => checkForLtol(m, accept));
+            }
+        }
+        
     }
 
     checkModel(model: Model, accept: ValidationAcceptor): void {
         if (model.commVars.length == 0) {
-            model.specs.forEach(ltol => { 
+            model.specs?.forEach(ltol => { 
                 if (ltol.quants !== undefined && ltol.quants.length > 0) {
                     accept("warning", `Quantified formula, but system has no property identifiers`, { node: ltol });
                 
                 }
             });
         }
+
+        model.guards?.forEach(g => checkForLtol(g.body, accept));
     }
 
 }
