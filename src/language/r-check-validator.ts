a363da9267b4bb4255efb1b58ab67830694dd35e
Luca Di Stefano <lucad@chalmers.se>
2024-01-04 09:37:05 +0100

    Improve validation of duplicate names

--- src/language/r-check-validator.ts
+++ src/language/r-check-validator.ts
@@ -1,4 +1,4 @@
-import { AstNode, ValidationAcceptor, ValidationChecks, isAstNode, streamAst, streamContents } from 'langium';
+import { AstNode, Stream, ValidationAcceptor, ValidationChecks, isAstNode, streamAst, streamContents } from 'langium';
 import { Agent, Model, RCheckAstType, isAssign, isBinExpr, isBox, isCommand, isDiamond, isExpr, isFinally, isGlobally, isNext } from './generated/ast.js';
 import type { RCheckServices } from './r-check-module.js';
 
@@ -29,19 +29,26 @@ function checkForLtol(node: AstNode, accept: ValidationAcceptor): void {
     }
 }
 
+function checkDuplicates(nodeArray: any[] | Stream<any> | undefined, what: string, accept: ValidationAcceptor) : Set<any> {
+    const seen = new Set();
+    nodeArray?.forEach(v => {
+        if (v.name !== undefined && seen.has(v.name)) {
+            accept("error", `Duplicate ${what} '${v.name}'`, {node: v, property: "name"});
+        }
+        seen.add(v.name);
+    });
+    return seen;
+}
+
 /**
  * Implementation of custom validations.
  */
 export class RCheckValidator {
     
     checkAgent(agent: Agent, accept: ValidationAcceptor): void {
-        const locals = new Set();
-        agent.locals?.forEach(v => {
-            if (locals.has(v.name)) {
-                accept("error", `Duplicate local variable '${v.name}'`, {node: v, property: "name"});
-            }
-            locals.add(v.name);
-        });
+        checkDuplicates(agent.locals, "local variable", accept);
+        checkDuplicates(streamAst(agent).filter(isCommand), "label", accept);
+
         if (agent.init !== undefined){
             checkForLtol(agent.init, accept);
         }
@@ -54,6 +61,14 @@ export class RCheckValidator {
     }
 
     checkModel(model: Model, accept: ValidationAcceptor): void {
+        checkDuplicates(model.channels, "channel name", accept);
+        checkDuplicates(model.msgStructs, "message variable", accept);
+        checkDuplicates(model.enums, "enum", accept);
+        model.guards?.forEach(g => checkDuplicates(g.params, "parameter", accept));
+        const cases = model.enums?.map(e => e.cases).flat()
+        checkDuplicates(cases, "enum case", accept);
+
+
         if (model.commVars.length == 0) {
             model.specs?.forEach(ltol => { 
                 if (ltol.quants !== undefined && ltol.quants.length > 0) {
