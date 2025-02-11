1012d1fcb67d77c51896d7a4088edbb5f4d5dd2a
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2025-02-11 16:17:16 +0100

    Improve verification command
    We now generate and show an HTML report.

--- src/extension/verify.ts
+++ src/extension/verify.ts
@@ -3,8 +3,7 @@ import * as path from 'node:path';
 import { promisify } from "node:util";
 import * as vscode from "vscode";
 import { Temp } from "./temp.js";
-import { mkdtempSync, writeFileSync } from "node:fs";
-import { tmpdir } from "node:os";
+import { writeFileSync } from "node:fs";
 import { integer } from "vscode-languageserver";
 import { jarCallback } from "./common.js";
 
@@ -29,10 +28,11 @@ export class Verify {
     Init(context: vscode.ExtensionContext): void {
         const args = ["--smv", "-tmp"]
         context.subscriptions.push(
-            vscode.commands.registerCommand('rcheck.verify', () => jarCallback(context, args, check, smvCallback))
+            vscode.commands.registerCommand(
+                'rcheck.verify', 
+                () => jarCallback(context, args, check, smvCallback))
         );
-        tmpDirName = mkdtempSync(path.join(tmpdir(), "rcheck-"));
-        temp.addDir(tmpDirName);
+        tmpDirName = temp.makeDir("rcheck-");
     }
 }
 
@@ -47,6 +47,8 @@ function check() {
  * Process SMV file
  */
 async function smvCallback(err: ExecFileException | null, _: string, stderr: string) {
+    const editor = vscode.window.visibleTextEditors.find((x) => x.document.fileName.endsWith("rcp"));
+    const fname = editor?.document.uri.fsPath.toString()!
     if (err) {
         vscode.window.showErrorMessage(err.message);
         return;
@@ -54,45 +56,75 @@ async function smvCallback(err: ExecFileException | null, _: string, stderr: str
     smvFile = stderr.trim();
     temp.addFile(smvFile);
 
-    execFile("grep", ["-B", "1", "^LTLSPEC", smvFile], grepCallback);
+    execFile(
+        "grep", ["-B", "1", "^LTLSPEC", smvFile],
+        (err, stdout, stderr) => grepCallback(fname, err, stdout, stderr)
+    );
 }
 
 /**
  * Launch verification tasks
  */
-async function grepCallback(err: ExecFileException | null, stdout: string, _: string) {
+async function grepCallback(fname:string, err: ExecFileException | null, stdout: string, _: string) {
     if (err) {
         vscode.window.showErrorMessage(err.message);
         return;
     }
-
     const specs = stdout.trim().replace("\n", "").split("--").slice(1)
     channel.show();
-    channel.appendLine("Model checking...");
-    Promise
-        .all(specs.map((element, index) => ic3(smvFile, index, element)))
-        .then((x) => { x.forEach((a) => channel.appendLine(a || ""))})
-        // .catch((x) => channel.appendLine(x))
-        .then(() => channel.appendLine("Done."))
-        .then(() => temp.cancel(smvFile));
+    channel.appendLine(`Model checking ${fname}...`);
+    const title = `R-CHECK: Verification of ${fname}`
+    let htmlReport =  `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+</head>
+<body>
+    <h1>${title}</h1>`
+    
+    await Promise
+        .all(specs.map(async (element, index) => {
+            const split = element.split("LTLSPEC").map((x) => x.trim());
+            const x = await ic3(smvFile, index, split[1]);
+            htmlReport = htmlReport.concat(formatOutput(split[0], x));
+        }))
+        .then(() => channel.appendLine("Done."));
+    const panel = vscode.window.createWebviewPanel(
+        "verificationResults",
+        "Verification Results",
+        vscode.ViewColumn.One
+    );
+    panel.webview.html = htmlReport + "</body></html>";
+}
+
+function formatOutput(spec: string, out: string) {
+    const isTrue = out.indexOf("is true") > -1
+    const isFalse = out.indexOf("is false") > -1
+    const emoji = isTrue ? "✅" : isFalse ? "❌" : "❔"
+
+    const lines = out
+        .split('\n')
+        .filter((x) => !x.startsWith("***") && !x.startsWith("-- no proof or counterexample found"))
+        .map((x) => x.trim())
+        .filter((x) => x);
+    return `<h2>${spec} ${emoji}</h2><pre>${lines.join("\n")}</pre>`;
 }
 
 /**
  * Verify an LTLSPEC property using IC3.
  * @param fname Name of the .smv file 
  * @param index Index of property being verified
- * @param spec The full text of the LTLSPEC being verified
+ * @param spec The LTLSPEC being verified
  */
 function ic3(fname: string, index: integer, spec: string, build_boolean_model: boolean=false) {
-    const split = spec.split("LTLSPEC").map((x) => x.trim());
-    // TODO a.  In some cases build_boolean_model is not needed and will actually
-    //          make the entire script fail, must detect & retry without it
-    //      b.  If spec starts with G one can use check_property_as_invar_ic3
+    // TODO If spec starts with G one can use check_property_as_invar_ic3
     const smvCommands = `
         set on_failure_script_quits 1
         go_msat
         ${build_boolean_model ? "build_boolean_model" : ""}
-        check_ltlspec_ic3 -p "${split[1]}"
+        check_ltlspec_ic3 -p "${spec}"
         quit`;
     const script = path.join(tmpDirName, `ltlspec-${index}.smv`);
     
@@ -103,6 +135,7 @@ function ic3(fname: string, index: integer, spec: string, build_boolean_model: b
         const child = execFile("nuxmv", ["-source", script, fname], async (err,stdout,stderr) => {
             if (err) {
                 if (err.message.indexOf("The boolean model must be built") > -1) {
+                    // Retry with build_boolean_model
                     await ic3(fname, index, spec, true).then(resolve, reject);
                 }
                 else {
@@ -112,6 +145,7 @@ function ic3(fname: string, index: integer, spec: string, build_boolean_model: b
             } else {
                 const stderrTrim = stderr.trim();
                 if (stderrTrim && stderrTrim.indexOf("The boolean model must be built") > -1) {
+                    // Retry with build_boolean_model
                     await ic3(fname, index, spec, true).then(resolve, reject);
                 }
                 else {
@@ -119,7 +153,7 @@ function ic3(fname: string, index: integer, spec: string, build_boolean_model: b
                     resolve(stdout);
                 }
             }
-            });
+        });
         temp.addChild(fname, child);
         
     });
