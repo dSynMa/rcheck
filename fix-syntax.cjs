198b700945286399aff98ee493c6bcbbd370ca82
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2026-02-23 15:05:40 +0100

    Fix syntax highlighting
    * Remove duplicate matches for true, exists, forall
    * Improve the structure of the new json file

--- fix-syntax.cjs
+++ fix-syntax.cjs
@@ -2,6 +2,7 @@ console.log("Fixing syntax hihglighting...")
 
 const fs = require('fs');
 const path = require('path');
+const { json } = require('stream/consumers');
 
 const jsonpath = path.join(__dirname, "syntaxes", "r-check.tmLanguage.json");
 
@@ -21,61 +22,96 @@ fs.readFile(jsonpath, 'utf8', (err, data) => {
             .filter(s => s.length > 0);
         oldPatterns.push(...match);
     });
-    // const oldPatterns = jsonData.patterns;
     console.log("Old patterns:", oldPatterns);
 
+
+    jsonData.repository.keywords = {
+        patterns: [
+            {
+                name: "keyword.control.r-check",
+                match: "\\b(SPEC|enum|channels|property-variables|guard|init|local|message-structure|receive-guard|relabel|rep|repeat|system)\\b|\\B(-automaton-state)\\b|\\b(GET@|SUPPLY@)\\B"
+            },
+            {  
+                match: "\\b(agent)\\b\\s+([^\\s]+)",
+                captures: {
+                    1: { name: "keyword.other.agent.r-check" },
+                    2: { name: "entity.name.agent.r-check" }
+                }
+            },
+            {
+                name: "keyword.operator.r-check",
+                match: "\\b(F|G|R|U|W|X|exists|forall)\\b"
+            },
+        ]};
+
+    jsonData.repository.constants = {
+        patterns: [
+            {
+                name: "constant.language.r-check",
+                match: "\\b(false|true|any)\\b"
+            },
+            {
+                name: "constant.numeric.r-check",
+                match: "\\b([0-9]+)\\b"
+            }
+        ]
+    }
+
+    jsonData.repository.variables = {
+        patterns: [
+            {
+                name: "variable.language.r-check",
+                match: "\\b(myself|chan|getter|p2p|sender|supplier)\\b"
+            }
+        ]
+    }
+
+    jsonData.repository.types = {
+        patterns: [
+            {
+                name: "storage.type.r-check",
+                match: "\\b(Agent|bool|channel|int|location)\\b"
+            }
+        ]
+    }
+
+    // Careful, this can still have false positives
+    oldPatterns.forEach(pattern => {
+        let found = false;
+        Object.keys(jsonData.repository).forEach(key => {
+            if (jsonData.repository[key].patterns.some(p => p.match !== undefined && p.match.includes(pattern))) {
+                found = true;
+                return;
+            }
+        });
+        if (!found) {
+            console.warn(`\x1b[33m[WARNING] Pattern "${pattern}" is missing in the new patterns.\x1b[0m`);
+        }
+    });
 
     jsonData.patterns = [
         {
             include: "#comments"
         },
         {
-            name: "keyword.control.r-check",
-            match: "\\b(SPEC|enum|channels|property-variables|exists|false|forall|guard|init|local|message-structure|receive-guard|relabel|rep|repeat|system)\\b|\\B(-automaton-state)\\b|\\b(GET@|SUPPLY@)\\B"
-        },
-        {  
-            match: "\\b(agent)\\b\\s+([^\\s]+)",
-            captures: {
-                1: { name: "keyword.other.agent.r-check" },
-                2: { name: "entity.name.agent.r-check" }
-            }
+            include: "#keywords"
         },
         {
-            name: "keyword.operator.r-check",
-            match: "\\b(F|G|R|U|W|X|exists|forall)\\b"
+            include: "#constants"
         },
         {
-            name: "constant.language.r-check",
-            match: "\\b(false|true|any)\\b"
+            include: "#variables"
         },
         {
-            name: "constant.numeric.r-check",
-            match: "\\b([0-9]+)\\b"
-        },
-        {
-            name: "variable.language.r-check",
-            match: "\\b(myself|chan|sender|getter|supplier|p2p)\\b"
-        },
-        {
-        name: "storage.type.r-check",
-        match: "\\b(bool|channel|int|Agent|location)\\b"
+            include: "#types"
         }
-    ];
-
-    // Careful, this can still have false positives
-    oldPatterns.forEach(pattern => {
-        if (!jsonData.patterns.some(p => p.match !== undefined && p.match.includes(pattern))) {
-            console.warn(`Pattern "${pattern}" is missing in the new patterns.`);
-        }
-    });
-
+    ]
 
     fs.writeFile(jsonpath, JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
         if (err) {
             console.error('Error writing file:', err);
             return;
         }
-        console.log('Syntax file updated successfully');
+        console.log('Syntax file updated successfully.');
     });
 });
-
